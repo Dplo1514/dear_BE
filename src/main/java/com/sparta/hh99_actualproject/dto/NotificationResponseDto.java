ff45a42b44dbdf7d977f0e0891cadcc5651278d6
Hyunggi <phg9587@naver.com>
2022-05-10 15:36:27 +0900

    Feat : 알람 관련 테이블 & Enum & Dto 추가

---
+++ src/main/java/com/sparta/hh99_actualproject/dto/NotificationResponseDto.java
@@ -0,0 +1,17 @@
+package com.sparta.hh99_actualproject.dto;
+
+import com.sparta.hh99_actualproject.model.NotiTypeEnum;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class NotificationResponseDto {
+    private NotiTypeEnum notiType;
+
+    private String notiContent;
+
+    private boolean isRead;
+}
---
+++ src/main/java/com/sparta/hh99_actualproject/model/NotiTypeEnum.java
@@ -0,0 +1,8 @@
+package com.sparta.hh99_actualproject.model;
+
+public enum NotiTypeEnum {
+    COMMENT, // 댓글 달림
+    FOLLOW,  // 팔로우 당함
+    CHOICE,  // 댓글 채택
+    MESSAGE; // 쪽지 도착
+}
