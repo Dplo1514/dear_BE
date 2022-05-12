e8f21c0acc9553988c1bec5fa6132bf45244ef76
Hyunggi <phg9587@naver.com>
2022-05-12 22:57:04 +0900

    Refact : Score 초기 Entity 생성시에 Score 초기값 설정 + 관련 코드들 리팩터링

--- src/main/java/com/sparta/hh99_actualproject/service/ScoreService.java
+++ src/main/java/com/sparta/hh99_actualproject/service/ScoreService.java
@@ -24,7 +24,6 @@ public class ScoreService {
             }
         }
         return tagCount;
-
     }
 
     public float getScoreValue(int tagCount , ReviewRequestDto reviewRequestDto ) {
@@ -32,7 +31,7 @@ public class ScoreService {
         return reviewRequestDto.isTagLike() ? (tagCount * 0.2f) : (tagCount * -0.5f);
     }
 
-    public void calculateMemberScore(String memberId, float scoreValue , ScoreType scoreType) {
+    public void calculateMemberScore(String memberId, float scoreValue, ScoreType scoreType) {
         float oppositeMemberScore = 36.5f; //초기 점수
         int oppositeMemberResponseChatCount = 0;
         int oppositeMemberRequestChatCount = 0;
@@ -40,26 +39,16 @@ public class ScoreService {
         //1. 기존 유저의 Score 가져옴
         Score oppositeMemberScoreModel = scoreRepository.findByMemberId(memberId)
                 .orElse(null);
-        //기존에 저장된게 없으므로 Table 생성이 필요
-        if(oppositeMemberScoreModel == null){
-            oppositeMemberScoreModel = scoreRepository.save(Score.of(memberId));
+
+        //기존에 저장된게 없으므로 Table 생성이 필요 ,
+        //생성된게 있으면 Get해서 기존 점수 가져와서 사용 후 새로운 점수를 적용해서 업데이트 필요
+        if (oppositeMemberScoreModel == null) {
+            oppositeMemberScoreModel = scoreRepository.save(Score.of(memberId, oppositeMemberScore));
         }
-        //생성된게 있으므로 새로운 점수를 적용해서 업데이트
-        else{
-            oppositeMemberScore = oppositeMemberScoreModel.getScore();
-            switch (scoreType){
-                case REQUEST_CHAT:
-                    oppositeMemberRequestChatCount = oppositeMemberScoreModel.getRequestChatCount();
-                    break;
-                case RESPONSE_CHAT:
-                    oppositeMemberResponseChatCount = oppositeMemberScoreModel.getResponseChatCount();
-                    break;
-                case COMMENT_SELECTION:
-                    oppositeMemberCommentSelectionCount = oppositeMemberScoreModel.getCommentSelectionCount();
-                    break;
-            }
-        }
-        oppositeMemberScore = oppositeMemberScore + scoreValue;
+
+        //oppositeMemberScore 점수 계산하기
+        oppositeMemberScore = oppositeMemberScoreModel.getScore();
+        oppositeMemberScore += scoreValue;
         //※ 마음 온도의 최대 상한은 100도 이다.
         if (oppositeMemberScore > 100f) {
             oppositeMemberScore = 100f;
@@ -67,14 +56,17 @@ public class ScoreService {
         //3. oppositeMemberScore 도 수정 반영 (좋은 점수면 + , 나쁜 점수면 -)
         oppositeMemberScoreModel.setScore(oppositeMemberScore);
         //4. 타입에 맞춰서 개수 추가 ( + 1)
-        switch (scoreType){
+        switch (scoreType) {
             case REQUEST_CHAT:
+                oppositeMemberRequestChatCount = oppositeMemberScoreModel.getRequestChatCount();
                 oppositeMemberScoreModel.setRequestChatCount(oppositeMemberRequestChatCount + 1);
                 break;
             case RESPONSE_CHAT:
+                oppositeMemberResponseChatCount = oppositeMemberScoreModel.getResponseChatCount();
                 oppositeMemberScoreModel.setResponseChatCount(oppositeMemberResponseChatCount + 1);
                 break;
             case COMMENT_SELECTION:
+                oppositeMemberCommentSelectionCount = oppositeMemberScoreModel.getCommentSelectionCount();
                 oppositeMemberScoreModel.setCommentSelectionCount(oppositeMemberCommentSelectionCount + 1);
                 break;
         }
