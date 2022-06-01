fffe1f68a862d05cd7e9d54b5b53207062c80680
Hyunggi <phg9587@naver.com>
2022-06-01 17:08:18 +0900

    Test : 무중단 배포 테스트

--- src/main/java/com/sparta/hh99_actualproject/controller/TestController.java
+++ src/main/java/com/sparta/hh99_actualproject/controller/TestController.java
@@ -3,13 +3,19 @@ package com.sparta.hh99_actualproject.controller;
 import com.sparta.hh99_actualproject.exception.PrivateResponseBody;
 import com.sparta.hh99_actualproject.exception.PrivateException;
 import com.sparta.hh99_actualproject.exception.StatusCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+
 @RestController
+@RequiredArgsConstructor
 public class TestController {
+    private final Environment env;
 
     @GetMapping("/api/test/getERR")
     public void getPosts(){
@@ -24,6 +30,14 @@ public class TestController {
     @GetMapping("/api/logintest")
     public ResponseEntity<PrivateResponseBody> getLoginTest(){
         return new ResponseEntity<>(new PrivateResponseBody(StatusCode.OK,"로그인 테스트 성공"), HttpStatus.OK);
+    }
+
+    //profile 조회
+    @GetMapping(value="/profile")
+    public String getProfile(){
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
     }
 
 }
